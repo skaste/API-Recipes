425483c17810dac80f4ef7c77f009670d111c5ac
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-08 14:48:08 -0600

    created the async await features for the app and will add the try catch into it

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-(console.log(`test`);
+// (console.log(`test`);
+
+const apiBaseURL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/`;
+
+const state = {
+  allRecipes: [],
+}
+const main = document.querySelector(`main`)
+
+
+const getAllRecipes = async () => {
+  const response = await fetch(apiBaseURL + "recipes");
+  // console.log(getAllRecipes);
+  const jsonResponse = await response.json();
+  state.allRecipes = jsonResponse.data
+console.log(jsonResponse);
+renderAllEvents();
+}
+
+const getRecipeDetails =  async (detailsOfRecipes) => {
+  const response = await fetch("${apiBaseURL}/recipes${id}");
+  const responseJson = await response.json(); 
+  const recipeDetails = responseJson.data
+  renderDetails();
+}
+
+
+
+getAllRecipes();
